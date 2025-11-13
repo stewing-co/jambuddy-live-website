4db5ee344d27fee51c8ed0b99d7b72d8d2906d03
Steve <sewing@warpandweftdata.com>
2025-11-13 11:00:26 -0500

    google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jambuddy.live',
   output: 'static',
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
