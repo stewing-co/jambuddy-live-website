acd5172d35b4a85b7cc1caa0a57e1e26b7761914
Steve <sewing@warpandweftdata.com>
2026-03-23 16:16:42 -0400

    ga helper

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jambuddy.live',
   output: 'static',
-  integrations: [
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-  ],
+  integrations: [],
   vite: {
     plugins: [tailwindcss()],
   },
